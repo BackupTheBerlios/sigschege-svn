875a7fbc24966adcba1aa5b97d17764820c69b30
ro_kenn <ro_kenn@54903100-e0df-0310-9d28-cfdd46c2f373>
2004-08-02 20:26:32 +0000

    added box class files

---
+++ trunk/EasyVec/src/EasyVecElmBox.cpp
@@ -0,0 +1,77 @@
+// -*- c++ -*-
+/// \file 
+ 
+// Copyright 2004 by Ulf Klaperski
+//
+// This file is part of EasyVec - Vector Figure Creation Library.
+// 
+// #############################################################################
+//
+// EasyVec is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// EasyVec is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with the EasyVec sources; see the file COPYING.  
+//
+// #############################################################################
+//
+// $Id: $
+
+#include "EasyVecElmBox.h"
+#include "EasyVec.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+EasyVecElmBox::EasyVecElmBox(EasyVecElmCompound* parent_compound, EasyVec* master_compound,
+                             EVPosInt upper_left, EVPosInt lower_right)
+  : EasyVecElm(parent_compound, master_compound),
+    elm_upper_left(upper_left),
+    elm_lower_right(lower_right) {};
+
+
+
+void EasyVecElmBox::draw(EasyVecView* view) {
+  int xscale;
+  EasyVec *testptr;
+  testptr->scale();
+  xscale = master->scale();
+  EVPosInt ul, lr;
+  EVPosInt ur, ll;
+
+  ul = elm_upper_left/xscale;
+  lr = elm_lower_right/xscale;
+
+  ur = EVPosInt(lr.xpos(), ul.ypos());
+  ll = EVPosInt(ul.xpos(), lr.ypos());
+  
+  view->draw_line(ul, ur, elm_pen_color);
+  view->draw_line(ur, lr, elm_pen_color);
+  view->draw_line(lr, ll, elm_pen_color);
+  view->draw_line(ll, ul, elm_pen_color);
+
+}
+
+void EasyVecElmBox::save_elm(ofstream &fig_file) {
+  vector<EVPosInt>::iterator points_iter;
+
+  fig_file << "2 1 0 1 " << elm_pen_color << " " << elm_fill_color << " " << elm_depth
+           << " 0 -1 0.0 0 0 0 0 0 " << 5 << endl;
+  fig_file << " ";
+  
+  fig_file << elm_upper_left.xpos()  << " " << elm_upper_left.ypos()
+           << elm_lower_right.xpos() << " " << elm_upper_left.ypos()
+           << elm_lower_right.xpos() << " " << elm_lower_right.ypos()
+           << elm_upper_left.xpos() << " " << elm_lower_right.ypos()
+           << elm_upper_left.xpos() << " " << elm_upper_left.ypos();
+
+  fig_file << endl;
+}
---
+++ trunk/EasyVec/src/EasyVecElmBox.h
@@ -0,0 +1,71 @@
+// -*- c++ -*-
+/// \file 
+ 
+// Copyright 2004 by Ulf Klaperski
+//
+// This file is part of EasyVec - Vector Figure Creation Library.
+// 
+// #############################################################################
+//
+// EasyVec is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+// 
+// EasyVec is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with the EasyVec sources; see the file COPYING.  
+//
+// #############################################################################
+//
+// $Id: $
+
+
+#ifndef _EASYVECELMBOX_H
+#define _EASYVECELMBOX_H _EASYVECELMBOX_H 
+
+#include "EVPosInt.h"
+#include "EasyVecElm.h"
+#include "EasyVecElmCompound.h"
+#include "EasyVecView.h"
+#include <vector>
+
+using namespace std;
+
+/// An EasyVec box element - corresponds to fig element polyline:box and polyline:arcbox
+class EasyVecElmBox : public EasyVecElm {
+public:
+  /// general constructor with no extra arguments
+  EasyVecElmBox(EasyVecElmCompound* parent_compound, EasyVec* master_compound)
+    : EasyVecElm(parent_compound, master_compound) {};
+  /// general constructor accepting corner points
+  EasyVecElmBox(EasyVecElmCompound* parent_compound, EasyVec* master_compound, EVPosInt upper_left, EVPosInt lower_right);
+
+  /// Return the limits of this element
+  virtual void get_limits(EVPosInt &upper_left, EVPosInt &lower_right) {
+    upper_left  = elm_upper_left;
+    lower_right = elm_lower_right;
+  }
+  /// set both corners of the box
+  void set_corners(EVPosInt upper_left, EVPosInt lower_right) {
+    upper_left  = elm_upper_left;
+    lower_right = elm_lower_right;
+  }
+  
+  vector<EasyVecElm*> flat_list() { vector<EasyVecElm*> res; res.push_back(this); return (res); };
+
+  /// send draw commands to the given view
+  virtual void draw(EasyVecView* view);
+  /// save element into a fig file
+  virtual void save_elm(ofstream &fig_file);
+
+private:
+  EVPosInt elm_upper_left, elm_lower_right;
+};
+
+#endif /* _EASYVECELMBOX_H */
+

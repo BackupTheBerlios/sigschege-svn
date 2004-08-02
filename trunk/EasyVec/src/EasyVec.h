055f0715a8fc299d56e5860b3f229f9f70f37e80
ro_kenn <ro_kenn@54903100-e0df-0310-9d28-cfdd46c2f373>
2004-08-02 22:24:27 +0000

    enhanced documentation a bit

--- trunk/EasyVec/src/EasyVec.h
+++ trunk/EasyVec/src/EasyVec.h
@@ -22,7 +22,7 @@
 //
 // #############################################################################
 //
-// $Id: $
+// $Id$
 
 
 #ifndef _EASYVEC_H
@@ -33,7 +33,18 @@
 #include "EasyVecElm.h"
 #include "EasyVecElmCompound.h"
        
+/// A vector figure class in Fig format
 
+/*!
+ * This class implements a complete vector figure in Fig format.
+ *
+ * It is derived from EasyVecElmCompound since it shares a lot of code with
+ * that class. Basically the figure is like an "invisible" compound.
+ *
+ * To create a figure, instantiate an object of this class.
+ * Functions for creating objects (box, polyline, text,...) are inherited from
+ * the EasyVecElmCompound class and explained there.
+ */
 class EasyVec : public EasyVecElmCompound {
 public:
   // constructor
@@ -44,17 +55,33 @@ public:
   void register_view(EasyVecView* view) { views.push_back(view); }
   void unregister_view(EasyVecView* view);
   bool set_screen_dpi(int new_screen_dpi);
+  /// Set the screen resolution in dpi
   int get_screen_dpi(void) { return screen_dpi; };
+  /// Set the file resolution - should not be used (see file_dpi variable)
   int get_file_dpi(void) { return file_dpi; };
+  /// Return the scaling factor file_dpi/screen_dpi
   int scale(void) { return scale_fact; }
   void build_views(void);
+  /// Save the figure in the fig file 'filename'
   bool save(string filename);
+
 private:
+  /// A flat list to be passed to views
   vector <EasyVecElm*> members_flat;
+  /// Indicate if members_flat is still valid or needs to be recreated
   bool members_flat_valid;
   bool auto_update;
+  /// List of views which show this figure
   vector<EasyVecView*> views;
-  int screen_dpi, file_dpi, scale_fact;
+  /// Resolution in fig file
+
+  /*!
+   * The default value of 1200 should not be changed, because that is what  
+   */
+  int file_dpi;
+  /// Resolutions for screen and file and the scaling factor between both
+  int screen_dpi;
+  int scale_fact;
 };
 
 #endif /* _EASYVEC_H */

361080af71da699b171d4127eea2814c582abae3
suupkopp <suupkopp@54903100-e0df-0310-9d28-cfdd46c2f373>
2004-12-13 21:22:39 +0000

    Sigschege: Extend the Python Interpreter, so that python scripts can be executed and Sigschege: a spam Module can be imported and be used.

--- trunk/src/Sigschege.cpp
+++ trunk/src/Sigschege.cpp
@@ -24,14 +24,36 @@
 // $Id: TimingMain.cpp 67 2004-12-05 23:05:10Z suupkopp $
 
 #include <Python.h>
+#include <stdio.h>
+
+extern "C" {
+
+  static PyObject* spam_Hello(PyObject *self, PyObject *args) {
+    printf("Hello\n");
+    return Py_BuildValue("i", 1);
+  }
+
+  static PyMethodDef SpamMethods[] = {
+    {"Hello",  spam_Hello, METH_VARARGS,"Output Hello."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+  };
+
+  PyMODINIT_FUNC initspam(void) {
+    (void) Py_InitModule("spam", SpamMethods);
+  }
+  
+}
 
 
 int main (int ARGC, char **ARGV) {
+  int ret;
 
   Py_Initialize();
-  PyRun_SimpleString("from time import time,ctime\n"
-                     "print 'Today is',ctime(time())\n");
+  initspam();
+
+  ret = Py_Main(ARGC, ARGV);
+  
   Py_Finalize();
 
-  return(0);
+  return(ret);
 }

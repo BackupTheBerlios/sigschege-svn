24c85e216a4812ad2e0e15c69479cfa22a17ad24
suupkopp <suupkopp@54903100-e0df-0310-9d28-cfdd46c2f373>
2005-01-09 20:16:08 +0000

    Sigschege: Add a Event variable type to the python interpreter.            This Type has no funtion yet, but is a starting point 	   to make the Python interface usable.

--- trunk/src/Sigschege.cpp
+++ trunk/src/Sigschege.cpp
@@ -42,6 +42,58 @@ extern "C" {
     (void) Py_InitModule("spam", SpamMethods);
   }
   
+  // add a new object type to python
+  typedef struct {
+    PyObject_HEAD
+  } sig_EventObject;
+  
+  static PyTypeObject sig_EventType = {
+    PyObject_HEAD_INIT(NULL)
+    0,                         /*ob_size*/
+    "sig.Event",               /*tp_name*/
+    sizeof(sig_EventObject),   /*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    0,                         /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
+    "Event",                   /*tp_doc */
+  };
+
+  static PyMethodDef sig_methods[] = {
+    {NULL}  /* Sentinel */
+  };
+
+  PyMODINIT_FUNC
+  initsig(void) 
+  {
+    PyObject* m;
+    
+    sig_EventType.tp_new = PyType_GenericNew;
+    if (PyType_Ready(&sig_EventType) < 0)
+        return;
+    
+    m = Py_InitModule3("sig", sig_methods,
+                       "Example Sigschege extentions.");
+    
+    Py_INCREF(&sig_EventType);
+    PyModule_AddObject(m, "Event", (PyObject *)&sig_EventType);
+  }
+
+
+
 }
 
 
@@ -50,7 +102,7 @@ int main (int ARGC, char **ARGV) {
 
   Py_Initialize();
   initspam();
-
+  initsig();
   ret = Py_Main(ARGC, ARGV);
   
   Py_Finalize();
